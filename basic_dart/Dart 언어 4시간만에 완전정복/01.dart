9715b75a6171d95fb2967f2df1cffad94b202bde
jueun choi <songjueun815@gmail.com>
2023-04-04 18:16:25 +0900

    변수

---
+++ basic_dart/Dart 언어 4시간만에 완전정복/01.dart
@@ -0,0 +1,10 @@
+/** VARIABLE */
+// 명시적
+String str1 = 'String';
+int int1 = 3;
+double double1 = 1.2;
+bool bool1 = true;
+// 암시적
+var var1 = 'var';       // 재할당 시 data type 변경 불가
+dynamic dynamic1 = 2.0; // 재할당 시 data type 변경 가능
+
