5c9d07084687fd52fbafc6533070b0286c0df18e
jueun choi <songjueun815@gmail.com>
2023-04-04 18:16:52 +0900

    암시적, 명시적 선언

--- basic_dart/Dart 언어 4시간만에 완전정복/01.dart
+++ basic_dart/Dart 언어 4시간만에 완전정복/01.dart
@@ -8,3 +8,7 @@ bool bool1 = true;
 var var1 = 'var';       // 재할당 시 data type 변경 불가
 dynamic dynamic1 = 2.0; // 재할당 시 data type 변경 가능
 
+// Example
+Map<String, Map<int, List<double>>> testType1 = {};
+var testType2 = {};
+
