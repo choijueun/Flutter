4969270c5475db192dd3a103dbd59a87bf72f109
jueun choi <songjueun815@gmail.com>
2023-04-05 17:06:46 +0900

    class...

---
+++ basic_dart/03/07_to_class.dart
@@ -0,0 +1,29 @@
+void main() {
+    List<Map<String, String>> people = [
+        {'name': 'AAA', 'dept': 'user'},
+        {'name': 'CTN', 'dept': 'R&B'},
+        {'name': 'MNA', 'dept': 'R&B'}
+    ];
+    print(people);
+    // [{name: AAA, dept: user}, {name: CTN, dept: R&B}, {name: MNA, dept: R&B}]
+
+    
+    final parsedPeople = people.map((x)=>Char(name:x['name']!,dept:x['dept']!));
+    print(parsedPeople);
+    // (Char[name:AAA;dept:user], Char[name:CTN;dept:R&B], Char[name:MNA;dept:R&B])
+}
+
+class Char {
+    final String name;
+    final String dept;
+
+    Char({
+        required this.name, 
+        required this.dept
+    });
+
+    @override
+    String toString(){
+        return 'Char[name:$name;dept:$dept]';
+    }
+}
