3192d8ef4ae41e505f95b9a19f4b62c4ced39ff0
jueun choi <songjueun815@gmail.com>
2023-04-05 16:37:27 +0900

    format

--- basic_dart/03/04_reduce.dart
+++ basic_dart/03/04_reduce.dart
@@ -1,25 +1,25 @@
 void main() {
-  List<int> numbers = [1, 3, 5, 7, 9];
-  print('=========== no1 ===========');
-  final result1 = numbers.reduce((prev, next){
-    print('prev: $prev & next: $next');
-    return prev;
-  });
-  print('result: $result1');
-  
-  print('=========== no2 ===========');
+    List<int> numbers = [1, 3, 5, 7, 9];
+    print('=========== no1 ===========');
+    final result1 = numbers.reduce((prev, next){
+        print('prev: $prev & next: $next');
+        return prev;
+    });
+    print('result: $result1');
+
+    print('=========== no2 ===========');
     final result2 = numbers.reduce((prev, next){
-    print('prev: $prev & next: $next');
-    return next;
-  });
-  print('result: $result2');
-  
-  print('=========== no3 ===========');
+        print('prev: $prev & next: $next');
+        return next;
+    });
+    print('result: $result2');
+
+    print('=========== no3 ===========');
     final result3 = numbers.reduce((prev, next){
-    print('prev: $prev & next: $next');
-    return prev+next;
-  });
-  print('result: $result3');
+        print('prev: $prev & next: $next');
+        return prev+next;
+    });
+    print('result: $result3');
 }
 
 /*
