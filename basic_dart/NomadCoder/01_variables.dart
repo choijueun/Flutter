2d098548f92b1f36de0892b6cb6d5ddf3b4a0c65
jueun choi <songjueun815@gmail.com>
2023-05-11 14:29:47 +0900

    variables

--- basic_dart/NomadCoder/01_variables.dart
+++ basic_dart/NomadCoder/01_variables.dart
@@ -1,3 +1,12 @@
 void main(){
-  
+  var name = 'N';
+  print(name);
+
+  String code = '0123';
+  print(code);
+
+  dynamic dCode = '0123';
+  print(dCode.runtimeType);
+  dCode = 123;
+  print(dCode.runtimeType);
 }
