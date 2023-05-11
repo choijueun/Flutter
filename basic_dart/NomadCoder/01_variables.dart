1478bb44111c922f8bd0b4e94472e75f56496592
jueun choi <songjueun815@gmail.com>
2023-05-11 17:38:48 +0900

    var - dynamic

--- basic_dart/NomadCoder/01_variables.dart
+++ basic_dart/NomadCoder/01_variables.dart
@@ -5,6 +5,15 @@ void main(){
   String code = '0123';
   print(code);
 
+  var dName;
+  if(dName is String){
+    print('이 블록 내에서는 dName. 메서드(String) 자동완성');
+  }
+  dName = 'A';
+  print(dName.runtimeType);
+  dName = 2.0;
+  print(dName.runtimeType);
+
   dynamic dCode = '0123';
   print(dCode.runtimeType);
   dCode = 123;
